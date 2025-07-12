3297e655e3ba41c2ede6f7024a4476383c70db56
bridgetorr17 <bridgetorr1902@gmail.com>
2025-07-12 19:52:50 -0400

    PUT request, updating jobs

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
 import AddJobPage from './pages/AddJobPage';
 import NotFoundPage from './pages/NotFoundPage';
+import EditJobPage from './pages/EditJobPage';
 import JobPage, { jobLoader } from './pages/JobPage';
 
 
@@ -31,12 +32,25 @@ const App = () => {
     return;
   }
 
+  //Update job
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(job)
+    });
+    return;
+  }
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path='/jobs' element={<JobsPage />}/>
         <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob} />} loader={jobLoader}/>
+        <Route path='/edit-job/:id' element={<EditJobPage updateJobSubmit={updateJob}/>} loader={jobLoader}/>
         <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />}/>
         <Route path="*" element={<NotFoundPage />}/>
       </Route>
