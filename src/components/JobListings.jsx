e75f5a2fc156923860f3bd6b268c8a8d8ecab136
bridgetorr17 <bridgetorr1902@gmail.com>
2025-07-12 17:00:42 -0400

    single job page

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -8,8 +8,9 @@ const JobListings = ({isHome=false}) => {
 
     useEffect(() => {
         const fetchJobs = async () => {
+            const limit = isHome ? '?_limit=3' : ''
             try{
-                const res = await fetch('http://localhost:8000/jobs')
+                const res = await fetch(`/api/jobs${limit}`)
                 const data = await res.json();
 
                 setJobs(data);
